e0a40463cbd1104bd5d1b8e356de84bea247a412
Paul Stevens <twinkisan@hotmail.com>
2020-10-28 21:38:08 -0700

    minor graphic tweaks

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 export default class Header extends Component {
     render() {
         return (
-            <div>
+            <header>
                 THIS IS MY HEADER, THERE ARE MANY LIKE IT BUT THIS ONE IS MINE
-            </div>
+            </header>
         )
     }
 }
