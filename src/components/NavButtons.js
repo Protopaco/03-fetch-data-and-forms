e6d3a775010d192b3e1453698b485dc4b55f9d5b
Paul Stevens <twinkisan@hotmail.com>
2020-11-01 15:34:33 -0800

    restyles nav-buttons

--- src/components/NavButtons.js
+++ src/components/NavButtons.js
@@ -1,27 +1,37 @@
 import React, { Component } from 'react'
+import { Link, } from 'react-router-dom';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import { ButtonGroup, Button } from '@material-ui/core';
+
 
 export default class NavButtons extends Component {
     render() {
         return (
-            <ul className="nav-list">
-                <li className='nav-listItems'>
-                    <button
-                        onClick={this.props.handlePreviousPageClick}
-                        disabled={this.props.currentPageState === 1}
-                    >previous
-                    </button>
-                </li>
-                <li className='nav-listItems'>
-                    <span className='nav-display'>{this.props.currentPageState} / {Math.ceil(this.props.totalReturnsState / this.props.resultsPerPageState)}</span>
-                </li>
-                <li className='nav-listItems'>
-                    <button
-                        onClick={this.props.handleNextPageClick}
-                        disabled={this.props.currentPageState === Math.ceil(this.props.totalReturnsState / this.props.resultsPerPageState)}
-                    >next
-                    </button>
-                </li>
-            </ul>
+            <ButtonGroup
+                variant="text"
+                color='primary'
+                size="large"
+                aria-label='contained primary button group'>
+                <IconButton
+                    component={Link}
+                    onClick={this.props.handlePreviousPageClick}
+                    disabled={this.props.currentPageState === 1}
+                >
+                    <ChevronLeft />
+                </IconButton>
+                <Button>
+                    {this.props.currentPageState} / {Math.ceil(this.props.totalReturnsState / this.props.resultsPerPageState)}
+                </Button>
+                <IconButton
+                    component={Link}
+                    onClick={this.props.handleNextPageClick}
+                    disabled={this.props.currentPageState === Math.ceil(this.props.totalReturnsState / this.props.resultsPerPageState)}
+                >
+                    <ChevronRight />
+                </IconButton>
+            </ButtonGroup>
         )
     }
 }
