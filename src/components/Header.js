6a756832bfb7e5174b89c97d781c95a8f1072f72
Paul Stevens <twinkisan@hotmail.com>
2020-10-31 20:33:31 -0700

    installs material design library

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import { primaryColorButton } from '../data/constants.js';
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+
 
 export default class Header extends Component {
     render() {
         return (
             <header>
-                <Link to="/"><span className="header-links">Home</span></Link>
-                <Link to="/Search"><span className="header-links">Search</span></Link>
+                <Link to="/">
+                    <Button variant="contained" style={primaryColorButton} >Home</Button>
+                </Link>
+                <Link to="/Search">
+                    <Button variant="contained" style={primaryColorButton}>
+                        Search</Button>
+                </Link>
             </header>
         )
     }
---
+++ src/data/constants.js
@@ -0,0 +1,8 @@
+
+
+export const primaryColorButton = {
+    backgroundColor: '#003A70',
+    color: '#FFCB05',
+    padding: '8px 15px',
+    margin: '5px',
+}
