715fb5c0a0b5e2c936be452b7e423d912838a9b7
Paul Stevens <twinkisan@hotmail.com>
2020-11-02 08:50:41 -0800

    fixed minor bug caused by IconButton components

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -3,12 +3,13 @@ import fetch from 'superagent';
 import { Link } from 'react-router-dom';
 import { Paper, Card, Typography, CardContent, Button } from '@material-ui/core';
 import { elementFrame, detailItemCard, detailItemImageDiv, detailItemCardBreakDiv, listItemCardContent } from '../data/constants.js';
+import { VictoryBar } from 'victory';
 
 
 export default class DetailsPage extends Component {
 
     state = {
-        pokemonObject: { pokemon: 'failure' }
+        pokemonObject: { pokemon: 'error' }
     }
 
     componentDidMount = async () => {
@@ -63,19 +64,7 @@ export default class DetailsPage extends Component {
                     </CardContent>
                 </Card>
 
-                {/* <div className='details-div' style={{ backgroundColor: this.state.pokemonObject.color_1, border: this.state.pokemonObject.color_2 }} >
-                    <ul className='details-ul'>
-                        <li className="details-info">
-                            <img className="details-image" src={this.state.pokemonObject.url_image} alt={this.state.pokemonObject.pokemon} />
-                        </li>
-                        <li className="details-info">Name: {this.state.pokemonObject.pokemon}</li>
-                        <li className="details-info">HP: {this.state.pokemonObject.hp}</li>
-                        <li className="details-info">Attack: {this.state.pokemonObject.attack}</li>
-                        <li className="details-info">Defense: {this.state.pokemonObject.defense}</li>
-                        <li className="details-info">Height: {this.state.pokemonObject.height}</li>
-                        <li className="details-info">Weight: {this.state.pokemonObject.weight}</li>
-                    </ul>
-                </div> */}
+
                 <Button variant='contained' color='primary' component={Link} to="/Search" style={{ marginTop: '0px' }}>back</Button>
             </Paper>
         )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-import IconButton from '@material-ui/core/IconButton';
 import HomeIcon from '@material-ui/icons/Home';
 import SearchIcon from '@material-ui/icons/Search';
 import { headerFrame } from '../data/constants.js';
-import { Paper, ButtonGroup } from '@material-ui/core';
+import { Paper, ButtonGroup, Button } from '@material-ui/core';
 
 
 export default class Header extends Component {
@@ -14,13 +13,13 @@ export default class Header extends Component {
             <Paper style={headerFrame}>
                 <ButtonGroup variant="text" color='primary' size="medium" aria-label='contained primary button group'>
 
-                    <IconButton component={Link} to='/' >
+                    <Button aria-label="home" component={Link} to='/' >
                         <HomeIcon />
-                    </IconButton>
+                    </Button>
 
-                    <IconButton component={Link} to='/Search' >
+                    <Button aria-label="search" component={Link} to='/Search' >
                         <SearchIcon />
-                    </IconButton>
+                    </Button>
                 </ButtonGroup>
             </Paper>
         )
